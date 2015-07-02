d93d826fd6f22ae63581490c22f8a2c0c4d59c62
James Talmage <james@talmage.io>
2015-07-02 17:37:50 -0400

    rename tests and move them around

--- test/test-with-server.js
+++ test/test-with-server.js
@@ -1,4 +1,4 @@
-describe('follow-redirects', function() {
+describe('follow-redirects ', function() {
   var express = require('express');
   var concat = require('concat-stream');
   var assert = require('assert');
@@ -41,65 +41,7 @@ describe('follow-redirects', function() {
       .nodeify(done);
   });
 
-  it('max Redirects defaults to 5', function(done) {
-    app.get('/a', redirectsTo('/b'));
-    app.get('/b', redirectsTo('/c'));
-    app.get('/c', redirectsTo('/d'));
-    app.get('/d', redirectsTo('/e'));
-    app.get('/e', redirectsTo('/f'));
-    app.get('/f', redirectsTo('/g'));
-    app.get('/g', sendsJson({foo:'bar'}));
-
-    server.start(app)
-      .then(asPromise(function(resolve, reject){
-        http.get('http://localhost:3600/a', resolve).on('error', reject);
-      }))
-      .catch(function(err) {
-        assert.ok(err.toString().match(/Max redirects exceeded/));
-      })
-      .nodeify(done);
-  });
-
-  it('max Redirects can be set globally', function(done) {
-    followRedirects.maxRedirects = 6;
-    app.get('/a', redirectsTo('/b'));
-    app.get('/b', redirectsTo('/c'));
-    app.get('/c', redirectsTo('/d'));
-    app.get('/d', redirectsTo('/e'));
-    app.get('/e', redirectsTo('/f'));
-    app.get('/f', redirectsTo('/g'));
-    app.get('/g', sendsJson({foo:'bar'}));
-
-    server.start(app)
-      .then(asPromise(function(resolve, reject){
-        http.get('http://localhost:3600/a', resolve).on('error', reject);
-      }))
-      .then(concatJson)
-      .then(function(json) {
-        assert.deepEqual(json, {foo:'bar'})
-      })
-      .nodeify(done);
-  });
-
-  it('max Redirects can be set per request', function(done) {
-    app.get('/a', redirectsTo('/b'));
-    app.get('/b', redirectsTo('/c'));
-    app.get('/c', sendsJson({foo:'bar'}));
-
-    var u = url.parse('http://localhost:3600/a');
-    u.maxRedirects = 1;
-
-    server.start(app)
-      .then(asPromise(function(resolve, reject){
-        http.get(u, resolve).on('error', reject);
-      }))
-      .catch(function(err) {
-        assert.ok(err.toString().match(/Max redirects exceeded/));
-      })
-      .nodeify(done);
-  });
-
-  it('will return 300 code if no location header set', function(done) {
+  it('should return with a 300 code if the response does not contain a location header', function(done) {
     app.get('/a', function(req, res){
       res.status(307).end();
     });
@@ -117,7 +59,7 @@ describe('follow-redirects', function() {
       .nodeify(done);
   });
 
-  it('will report an error', function(done) {
+  it('should emit connection errors on the returned stream', function(done) {
     app.get('/a', redirectsTo('http://localhost:36002/b'));
 
     server.start(app)
@@ -130,7 +72,7 @@ describe('follow-redirects', function() {
       .nodeify(done);
   });
 
-  it('works using https', function(done) {
+  it('should follow redirects over https', function(done) {
     app.get('/a', redirectsTo('/b'));
     app.get('/b', redirectsTo('/c'));
     app.get('/c', sendsJson({baz:'quz'}));
@@ -148,43 +90,7 @@ describe('follow-redirects', function() {
       .nodeify(done);
   });
 
-  it('https -> http -> https', function(done) {
-    app.get('/a', redirectsTo('http://localhost:3600/b'));
-    app2.get('/b', redirectsTo('https://localhost:3601/c'));
-    app.get('/c', sendsJson({yes:'no'}));
-
-    Promise.all([server.start(app,'https'), server.start(app2)])
-      .then(asPromise(function(resolve, reject){
-        var opts = url.parse('https://localhost:3601/a');
-        server.addClientCerts(opts);
-        https.get(opts, resolve).on('error', reject);
-      }))
-      .then(concatJson)
-      .then(function(json) {
-        assert.deepEqual(json, {yes:'no'});
-      })
-      .nodeify(done);
-  });
-
-  it('http -> https -> http', function(done) {
-    app.get('/a', redirectsTo('https://localhost:3601/b'));
-    app2.get('/b', redirectsTo('http://localhost:3600/c'));
-    app.get('/c', sendsJson({hello:'goodbye'}));
-
-    Promise.all([server.start(app), server.start(app2, 'https')])
-      .then(asPromise(function(resolve, reject){
-        var opts = url.parse('http://localhost:3600/a');
-        server.addClientCerts(opts);
-        http.get(opts, resolve).on('error', reject);
-      }))
-      .then(concatJson)
-      .then(function(json) {
-        assert.deepEqual(json, {hello:'goodbye'});
-      })
-      .nodeify(done);
-  });
-
-  it('redirect to query params', function(done) {
+  it('should honor query params in redirects', function(done) {
     app.get('/a', redirectsTo('/b?greeting=hello'));
     app.get('/b', function(req, res) {
       res.json({greeting:req.query.greeting});
@@ -199,6 +105,104 @@ describe('follow-redirects', function() {
         assert.deepEqual(json, {greeting:'hello'})
       })
       .nodeify(done);
+  });
+
+  describe('should obey a `maxRedirects` property ', function() {
+    it('which defaults to 5', function(done) {
+      app.get('/a', redirectsTo('/b'));
+      app.get('/b', redirectsTo('/c'));
+      app.get('/c', redirectsTo('/d'));
+      app.get('/d', redirectsTo('/e'));
+      app.get('/e', redirectsTo('/f'));
+      app.get('/f', redirectsTo('/g'));
+      app.get('/g', sendsJson({foo:'bar'}));
+
+      server.start(app)
+        .then(asPromise(function(resolve, reject){
+          http.get('http://localhost:3600/a', resolve).on('error', reject);
+        }))
+        .catch(function(err) {
+          assert.ok(err.toString().match(/Max redirects exceeded/));
+        })
+        .nodeify(done);
+    });
+
+    it('which can be set globally', function(done) {
+      followRedirects.maxRedirects = 6;
+      app.get('/a', redirectsTo('/b'));
+      app.get('/b', redirectsTo('/c'));
+      app.get('/c', redirectsTo('/d'));
+      app.get('/d', redirectsTo('/e'));
+      app.get('/e', redirectsTo('/f'));
+      app.get('/f', redirectsTo('/g'));
+      app.get('/g', sendsJson({foo:'bar'}));
+
+      server.start(app)
+        .then(asPromise(function(resolve, reject){
+          http.get('http://localhost:3600/a', resolve).on('error', reject);
+        }))
+        .then(concatJson)
+        .then(function(json) {
+          assert.deepEqual(json, {foo:'bar'})
+        })
+        .nodeify(done);
+    });
+
+    it('set as an option on an individual request', function(done) {
+      app.get('/a', redirectsTo('/b'));
+      app.get('/b', redirectsTo('/c'));
+      app.get('/c', sendsJson({foo:'bar'}));
+
+      var u = url.parse('http://localhost:3600/a');
+      u.maxRedirects = 1;
+
+      server.start(app)
+        .then(asPromise(function(resolve, reject){
+          http.get(u, resolve).on('error', reject);
+        }))
+        .catch(function(err) {
+          assert.ok(err.toString().match(/Max redirects exceeded/));
+        })
+        .nodeify(done);
+    });
+  });
+
+  describe ('should handle cross protocol redirects ', function() {
+    it('(https -> http -> https)', function(done) {
+      app.get('/a', redirectsTo('http://localhost:3600/b'));
+      app2.get('/b', redirectsTo('https://localhost:3601/c'));
+      app.get('/c', sendsJson({yes:'no'}));
+
+      Promise.all([server.start(app,'https'), server.start(app2)])
+        .then(asPromise(function(resolve, reject){
+          var opts = url.parse('https://localhost:3601/a');
+          server.addClientCerts(opts);
+          https.get(opts, resolve).on('error', reject);
+        }))
+        .then(concatJson)
+        .then(function(json) {
+          assert.deepEqual(json, {yes:'no'});
+        })
+        .nodeify(done);
+    });
+
+    it('(http -> https -> http)', function(done) {
+      app.get('/a', redirectsTo('https://localhost:3601/b'));
+      app2.get('/b', redirectsTo('http://localhost:3600/c'));
+      app.get('/c', sendsJson({hello:'goodbye'}));
+
+      Promise.all([server.start(app), server.start(app2, 'https')])
+        .then(asPromise(function(resolve, reject){
+          var opts = url.parse('http://localhost:3600/a');
+          server.addClientCerts(opts);
+          http.get(opts, resolve).on('error', reject);
+        }))
+        .then(concatJson)
+        .then(function(json) {
+          assert.deepEqual(json, {hello:'goodbye'});
+        })
+        .nodeify(done);
+    });
   });
 
   function redirectsTo(opt_status, path) {
