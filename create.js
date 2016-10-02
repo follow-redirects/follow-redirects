follow-redirects/follow-redirects

commit b4554ff196815141bd51291fabda2b23026c8e48

Restore the response event. (#31)
* Restore the response event.

This makes the response event and callback behave as in the Node.js API.
Fixes #30.

* Simplify callback passing.

create.js
@@ -22,11 +22,18 @@ module.exports = function(_nativeProtocols) {
 
   return publicApi;
 
-  function execute(options) {
-    var clientRequest;
+  function execute(options, callback) {
     var fetchedUrls = [];
+    var clientRequest = cb();
 
-    return (clientRequest = cb());
+    // return a proxy to the request with separate event handling
+    var requestProxy = Object.create(clientRequest);
+    requestProxy._events = {};
+    requestProxy._eventsCount = 0;
+    if (callback) {
+      requestProxy.on('response', callback);
+    }
+    return requestProxy;
 
     function cb(res) {
       // skip the redirection logic on the first call.
@@ -36,7 +43,8 @@ module.exports = function(_nativeProtocols) {
 
         if (!isRedirect(res)) {
           res.fetchedUrls = fetchedUrls;
-          return options.userCallback(res);
+          requestProxy.emit('response', res);
+          return;
         }
 
         // we are going to follow the redirect, but in node 0.10 we must first attach a data listener
@@ -61,10 +69,7 @@ module.exports = function(_nativeProtocols) {
       options.defaultRequest = defaultMakeRequest;
 
       var req = (options.makeRequest || defaultMakeRequest)(options, cb, res);
-
-      if (res) {
-        req.on('error', forwardError);
-      }
+      req.on('error', forwardError);
       return req;
     }
 
@@ -87,7 +92,7 @@ module.exports = function(_nativeProtocols) {
     // bubble errors that occur on the redirect back up to the initiating client request
     // object, otherwise they wind up killing the process.
     function forwardError (err) {
-      clientRequest.emit('error', err);
+      requestProxy.emit('error', err);
     }
   }
 
@@ -99,13 +104,12 @@ module.exports = function(_nativeProtocols) {
     publicApi[scheme] = H;
 
     H.request = function(options, callback) {
-      return execute(parseOptions(options, callback, wrappedProtocol));
+      return execute(parseOptions(options, wrappedProtocol), callback);
     };
 
     // see https://github.com/joyent/node/blob/master/lib/http.js#L1623
     H.get = function(options, callback) {
-      options = parseOptions(options, callback, wrappedProtocol);
-      var req = execute(options);
+      var req = execute(parseOptions(options, wrappedProtocol), callback);
       req.end();
       return req;
     };
@@ -113,8 +117,7 @@ module.exports = function(_nativeProtocols) {
 
   // returns a safe copy of options (or a parsed url object if options was a string).
   // validates that the supplied callback is a function
-  function parseOptions (options, callback, wrappedProtocol) {
-    assert.equal(typeof callback, 'function', 'callback must be a function');
+  function parseOptions (options, wrappedProtocol) {
     if ('string' === typeof options) {
       options = url.parse(options);
       options.maxRedirects = publicApi.maxRedirects;
@@ -125,8 +128,6 @@ module.exports = function(_nativeProtocols) {
       }, options);
     }
     assert.equal(options.protocol, wrappedProtocol, 'protocol mismatch');
-    options.protocol = wrappedProtocol;
-    options.userCallback = callback;
 
     debug('options', options);
     return options;

test/test-with-server.js
@@ -39,7 +39,7 @@ describe('follow-redirects ', function() {
     server.stop().nodeify(done);
   });
 
-  it('http.get', function(done) {
+  it('http.get with callback', function(done) {
     app.get('/a', redirectsTo('/b'));
     app.get('/b', redirectsTo('/c'));
     app.get('/c', redirectsTo('/d'));
@@ -50,6 +50,34 @@ describe('follow-redirects ', function() {
     server.start(app)
       .then(asPromise(function(resolve, reject){
         http.get('http://localhost:3600/a', concatJson(resolve, reject)).on('error', reject);
+      }))
+      .then(function(res) {
+        assert.deepEqual(res.parsedJson, {a:'b'});
+        assert.deepEqual(res.fetchedUrls, [
+          'http://localhost:3600/f',
+          'http://localhost:3600/e',
+          'http://localhost:3600/d',
+          'http://localhost:3600/c',
+          'http://localhost:3600/b',
+          'http://localhost:3600/a'
+        ]);
+      })
+      .nodeify(done);
+  });
+
+  it('http.get with response event', function(done) {
+    app.get('/a', redirectsTo('/b'));
+    app.get('/b', redirectsTo('/c'));
+    app.get('/c', redirectsTo('/d'));
+    app.get('/d', redirectsTo('/e'));
+    app.get('/e', redirectsTo('/f'));
+    app.get('/f', sendsJson({a:'b'}));
+
+    server.start(app)
+      .then(asPromise(function(resolve, reject){
+        http.get('http://localhost:3600/a')
+            .on('response', concatJson(resolve, reject))
+            .on('error', reject);
       }))
       .then(function(res) {
         assert.deepEqual(res.parsedJson, {a:'b'});

commit f1a58e8e02f4c726e13f7ac6895da89c4c7201d8

Proxy native requests with a separate object.
Fixes connection closing issues introduced
by b4554ff196815141bd51291fabda2b23026c8e48.
Closes #34.
Closes #37.

create.js
@@ -1,8 +1,43 @@
 'use strict';
 var url = require('url');
 var assert = require('assert');
+var Writable = require('stream').Writable;
 var debug = require('debug')('follow-redirects');
 var consume = require('stream-consume');
+
+// Wrapper around the native request
+function RequestProxy() {
+	Writable.call(this);
+}
+RequestProxy.prototype = Object.create(Writable.prototype);
+
+RequestProxy.prototype.abort = function () {
+	this._request.abort();
+};
+
+RequestProxy.prototype.end = function (data, encoding, callback) {
+	this._request.end(data, encoding, callback);
+};
+
+RequestProxy.prototype.flushHeaders = function () {
+	this._request.flushHeaders();
+};
+
+RequestProxy.prototype.setNoDelay = function (noDelay) {
+	this._request.setNoDelay(noDelay);
+};
+
+RequestProxy.prototype.setSocketKeepAlive = function (enable, initialDelay) {
+	this._request.setSocketKeepAlive(enable, initialDelay);
+};
+
+RequestProxy.prototype.setTimeout = function (timeout, callback) {
+	this._request.setSocketKeepAlive(timeout, callback);
+};
+
+RequestProxy.prototype._write = function (chunk, encoding, callback) {
+	this._request.write(chunk, encoding, callback);
+};
 
 module.exports = function (_nativeProtocols) {
 	var nativeProtocols = {};
@@ -24,15 +59,11 @@ module.exports = function (_nativeProtocols) {
 
 	function execute(options, callback) {
 		var fetchedUrls = [];
-		var clientRequest = cb();
-
-		// return a proxy to the request with separate event handling
-		var requestProxy = Object.create(clientRequest);
-		requestProxy._events = {};
-		requestProxy._eventsCount = 0;
+		var requestProxy = new RequestProxy();
 		if (callback) {
 			requestProxy.on('response', callback);
 		}
+		cb();
 		return requestProxy;
 
 		function cb(res) {
@@ -70,6 +101,7 @@ module.exports = function (_nativeProtocols) {
 
 			var req = (options.makeRequest || defaultMakeRequest)(options, cb, res);
 			req.on('error', forwardError);
+			requestProxy._request = req;
 			return req;
 		}
 