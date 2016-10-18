92fad4e6c1de9008082226b6daf9c1af52734433
Ruben Verborgh <ruben@verborgh.org>
2016-10-18 15:06:08 +0900

    Move independent functions out of export block.

--- create.js
+++ create.js
@@ -5,6 +5,11 @@ var Writable = require('stream').Writable;
 var debug = require('debug')('follow-redirects');
 var consume = require('stream-consume');
 
+var publicApi = {
+	maxRedirects: 5
+};
+var nativeProtocols = {};
+
 // Wrapper around the native request
 function RequestProxy() {
 	Writable.call(this);
@@ -39,137 +44,99 @@ RequestProxy.prototype._write = function (chunk, encoding, callback) {
 	this._request.write(chunk, encoding, callback);
 };
 
-module.exports = function (_nativeProtocols) {
-	var nativeProtocols = {};
-
-	var publicApi = {
-		maxRedirects: 5
-	};
-
-	for (var p in _nativeProtocols) {
-		/* istanbul ignore else */
-		if (_nativeProtocols.hasOwnProperty(p)) {
-			// http://www.ietf.org/rfc/rfc2396.txt - Section 3.1
-			assert(/^[A-Z][A-Z\+\-\.]*$/i.test(p), JSON.stringify(p) + ' is not a valid scheme name');
-			generateWrapper(p, _nativeProtocols[p]);
-		}
+function execute(options, callback) {
+	var fetchedUrls = [];
+	var requestProxy = new RequestProxy();
+	if (callback) {
+		requestProxy.on('response', callback);
 	}
+	cb();
+	return requestProxy;
 
-	return publicApi;
+	function cb(res) {
+		// skip the redirection logic on the first call.
+		if (res) {
+			var fetchedUrl = url.format(options);
+			fetchedUrls.unshift(fetchedUrl);
 
-	function execute(options, callback) {
-		var fetchedUrls = [];
-		var requestProxy = new RequestProxy();
-		if (callback) {
-			requestProxy.on('response', callback);
-		}
-		cb();
-		return requestProxy;
-
-		function cb(res) {
-			// skip the redirection logic on the first call.
-			if (res) {
-				var fetchedUrl = url.format(options);
-				fetchedUrls.unshift(fetchedUrl);
-
-				if (!isRedirect(res)) {
-					res.fetchedUrls = fetchedUrls;
-					requestProxy.emit('response', res);
-					return;
-				}
-
-				// we are going to follow the redirect, but in node 0.10 we must first attach a data listener
-				// to consume the stream and send the 'end' event
-				consume(res);
-
-				// need to use url.resolve() in case location is a relative URL
-				var redirectUrl = url.resolve(fetchedUrl, res.headers.location);
-				debug('redirecting to', redirectUrl);
-
-				// clean all the properties related to the old url away, and copy from the redirect url
-				wipeUrlProps(options);
-				extend(options, url.parse(redirectUrl));
-			}
-
-			if (fetchedUrls.length > options.maxRedirects) {
-				var err = new Error('Max redirects exceeded.');
-				requestProxy.emit('error', err);
+			if (!isRedirect(res)) {
+				res.fetchedUrls = fetchedUrls;
+				requestProxy.emit('response', res);
 				return;
 			}
 
-			options.nativeProtocol = nativeProtocols[options.protocol];
-			options.defaultRequest = defaultMakeRequest;
+			// we are going to follow the redirect, but in node 0.10 we must first attach a data listener
+			// to consume the stream and send the 'end' event
+			consume(res);
 
-			var req = (options.makeRequest || defaultMakeRequest)(options, cb, res);
-			requestProxy._request = req;
-			mirrorEvent(req, 'abort');
-			mirrorEvent(req, 'aborted');
-			mirrorEvent(req, 'error');
-			return req;
+			// need to use url.resolve() in case location is a relative URL
+			var redirectUrl = url.resolve(fetchedUrl, res.headers.location);
+			debug('redirecting to', redirectUrl);
+
+			// clean all the properties related to the old url away, and copy from the redirect url
+			wipeUrlProps(options);
+			extend(options, url.parse(redirectUrl));
 		}
 
-		function defaultMakeRequest(options, cb, res) {
-			if (res && res.statusCode !== 307) {
-				// This is a redirect, so use only GET methods, except for status 307,
-				// which must honor the previous request method.
-				options.method = 'GET';
-			}
-
-			var req = options.nativeProtocol.request(options, cb);
-
-			if (res) {
-				// We leave the user to call `end` on the first request
-				req.end();
-			}
-
-			return req;
+		if (fetchedUrls.length > options.maxRedirects) {
+			var err = new Error('Max redirects exceeded.');
+			requestProxy.emit('error', err);
+			return;
 		}
 
-		// send events through the proxy
-		function mirrorEvent(req, event) {
-			req.on(event, function (arg) {
-				requestProxy.emit(event, arg);
-			});
-		}
+		options.nativeProtocol = nativeProtocols[options.protocol];
+		options.defaultRequest = defaultMakeRequest;
+
+		var req = (options.makeRequest || defaultMakeRequest)(options, cb, res);
+		requestProxy._request = req;
+		mirrorEvent(req, 'abort');
+		mirrorEvent(req, 'aborted');
+		mirrorEvent(req, 'error');
+		return req;
 	}
 
-	function generateWrapper(scheme, nativeProtocol) {
-		var wrappedProtocol = scheme + ':';
-		var H = function () {};
-		H.prototype = nativeProtocols[wrappedProtocol] = nativeProtocol;
-		H = new H();
-		publicApi[scheme] = H;
+	function defaultMakeRequest(options, cb, res) {
+		if (res && res.statusCode !== 307) {
+			// This is a redirect, so use only GET methods, except for status 307,
+			// which must honor the previous request method.
+			options.method = 'GET';
+		}
 
-		H.request = function (options, callback) {
-			return execute(parseOptions(options, wrappedProtocol), callback);
-		};
+		var req = options.nativeProtocol.request(options, cb);
 
-		// see https://github.com/joyent/node/blob/master/lib/http.js#L1623
-		H.get = function (options, callback) {
-			var req = execute(parseOptions(options, wrappedProtocol), callback);
+		if (res) {
+			// We leave the user to call `end` on the first request
 			req.end();
-			return req;
-		};
-	}
-
-	// returns a safe copy of options (or a parsed url object if options was a string).
-	// validates that the supplied callback is a function
-	function parseOptions(options, wrappedProtocol) {
-		if (typeof options === 'string') {
-			options = url.parse(options);
-			options.maxRedirects = publicApi.maxRedirects;
-		} else {
-			options = extend({
-				maxRedirects: publicApi.maxRedirects,
-				protocol: wrappedProtocol
-			}, options);
 		}
-		assert.equal(options.protocol, wrappedProtocol, 'protocol mismatch');
 
-		debug('options', options);
-		return options;
+		return req;
 	}
-};
+
+	// send events through the proxy
+	function mirrorEvent(req, event) {
+		req.on(event, function (arg) {
+			requestProxy.emit(event, arg);
+		});
+	}
+}
+
+// returns a safe copy of options (or a parsed url object if options was a string).
+// validates that the supplied callback is a function
+function parseOptions(options, wrappedProtocol) {
+	if (typeof options === 'string') {
+		options = url.parse(options);
+		options.maxRedirects = publicApi.maxRedirects;
+	} else {
+		options = extend({
+			maxRedirects: publicApi.maxRedirects,
+			protocol: wrappedProtocol
+		}, options);
+	}
+	assert.equal(options.protocol, wrappedProtocol, 'protocol mismatch');
+
+	debug('options', options);
+	return options;
+}
 
 // copies source's own properties onto destination and returns destination
 function extend(destination, source) {
@@ -199,3 +166,35 @@ function wipeUrlProps(options) {
 		options[urlProps[i]] = null;
 	}
 }
+
+module.exports = function (_nativeProtocols) {
+	for (var p in _nativeProtocols) {
+		/* istanbul ignore else */
+		if (_nativeProtocols.hasOwnProperty(p)) {
+			// http://www.ietf.org/rfc/rfc2396.txt - Section 3.1
+			assert(/^[A-Z][A-Z\+\-\.]*$/i.test(p), JSON.stringify(p) + ' is not a valid scheme name');
+			generateWrapper(p, _nativeProtocols[p]);
+		}
+	}
+
+	return publicApi;
+
+	function generateWrapper(scheme, nativeProtocol) {
+		var wrappedProtocol = scheme + ':';
+		var H = function () {};
+		H.prototype = nativeProtocols[wrappedProtocol] = nativeProtocol;
+		H = new H();
+		publicApi[scheme] = H;
+
+		H.request = function (options, callback) {
+			return execute(parseOptions(options, wrappedProtocol), callback);
+		};
+
+		// see https://github.com/joyent/node/blob/master/lib/http.js#L1623
+		H.get = function (options, callback) {
+			var req = execute(parseOptions(options, wrappedProtocol), callback);
+			req.end();
+			return req;
+		};
+	}
+};
