c5d5b713986eec02c6de48204032eaa8c6e117e6
Ruben Verborgh <ruben@verborgh.org>
2016-10-21 19:58:29 +0900

    Remove Node.js 0.8.0 code.

--- test/lib/https-config.js
+++ test/lib/https-config.js
@@ -1,10 +1,8 @@
 var path = require('path');
 var fs = require('fs');
-var node8 = require('semver').lt(process.version, '0.9.0');
 
 function addServerOptions(options) {
 	options = options || {};
-	// options.ca = [fs.readFileSync(__dirname + '/TestCA.crt')];
 	options.cert = fs.readFileSync(path.join(__dirname, 'TestServer.crt'));
 	options.key = fs.readFileSync(path.join(__dirname, 'TestServer.pem'));
 	return options;
@@ -13,14 +11,7 @@ function addServerOptions(options) {
 function addClientOptions(options) {
 	options = options || {};
 	options.ca = [fs.readFileSync(path.join(__dirname, 'TestCA.crt'))];
-	// options.cert = fs.readFileSync(__dirname + '/TestClient.crt');
-	// options.key = fs.readFileSync(__dirname + '/TestClient.pem') ;
-
-	if (node8) {
-		options.agent = new options.nativeProtocol.Agent(options);
-	} else {
-		options.agent = false;
-	}
+	options.agent = false;
 	return options;
 }
 
